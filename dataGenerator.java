ad3e96aa3fb7dac7296b9b8f4f9e1e1bc9ab13f9
s3568806 <s3568806@student.rmit.edu.au>
2017-08-31 13:59:22 +1000

    Update dataGenerator.java
    random data is now saved to a input file called "randomsample.in"

--- dataGenerator.java
+++ dataGenerator.java
@@ -8,11 +8,20 @@ public class dataGenerator{
 			System.exit(1);
 		}
 		
-		int size = Integer.parseInt(args[0]);
-		Random mRandGen = new Random();
-		for (int i = 0; i < size; i ++){
-			int n = mRandGen.nextInt(100) + 1;		
-			System.out.print(n + " ");
+		try{
+			PrintWriter writer = new PrintWriter("randomsample.in", "UTF-8");
+			int size = Integer.parseInt(args[0]);
+			Random mRandGen = new Random();
+			for (int i = 0; i < size; i ++){
+				int n = mRandGen.nextInt(10000) + 1;	
+				System.out.print(n + " ");
+				writer.println(n);
+			}
+			writer.close();
+		}
+		catch(IOException e){
+			System.out.println("file failed to create");
+			System.exit(1);
 		}
 	}
 }
