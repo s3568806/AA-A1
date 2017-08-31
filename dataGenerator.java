655a4cdc382af0ac83e564dda7d5ebd9dd56c316
s3568806 <s3568806@student.rmit.edu.au>
2017-08-31 13:44:35 +1000

    Update dataGenerator.java
    Generator now samples user specified number of 1 to 100

--- dataGenerator.java
+++ dataGenerator.java
@@ -2,53 +2,17 @@ import java.io.*;
 import java.util.Random;
 
 public class dataGenerator{
-	/** Program name. */
-	protected static final String progName = "DataGenerator";
-	/** Random generator to use. */
-	Random mRandGen;
-
 	public static void main(String[] args){
-		// check correct number of command line arguments	
-		try {
-			// integer range
-			int startOfRange = Integer.parseInt(args[0]);
-			int endOfRange = Integer.parseInt(args[1]);
-			
-			// number of values to sample
-			int sampleSize = Integer.parseInt(args[2]);
-			
-			// type of sampling
-			String samplingType = args[3];
-			
-			dataGenerator gen = new dataGenerator(startOfRange, endOfRange);
-			
-			int[] samples = null;
-			switch (samplingType) {
-				// sampling with replacement
-				case "with":
-					samples = gen.sampleWithReplacement(sampleSize);
-					break;
-				// sampling without replacement
-				case "without":
-					samples = gen.sampleWithOutReplacement(sampleSize);
-					break;
-				default:
-					System.err.println(samplingType + " is an unknown sampling type.");
-					usage();
-			}
-			
-			// print out samples
-			if (samples != null) {
-				for (int i = 0; i < samples.length; i++) {
-					System.out.print(samples[i] + " ");
-				}
-				System.out.println("");
-			}
-			
+		if(args.length != 1){
+			System.out.println("number of arguments insufficient");
+			System.exit(1);
 		}
-		catch (Exception e) {
-			System.err.println(e.getMessage());
-			usage();
+		
+		int size = Integer.parseInt(args[0]);
+		Random mRandGen = new Random();
+		for (int i = 0; i < size; i ++){
+			int n = mRandGen.nextInt(100) + 1;		
+			System.out.print(n + " ");
 		}
 	}
 }
